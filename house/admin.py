38af7a07274dcd0fef91a374dbf042e89309afd7
timeiscoal <timeiscoal@gmail.com>
2022-11-18 20:42:02 +0900

    feat : house apps add admin and models

--- house/admin.py
+++ house/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
-
+from .models import House
 # Register your models here.
+
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+
+    fields = (
+        "name",
+        "description",
+        ("address", "price",),
+        "pets_allowed",
+    )
+
+    list_display = (
+
+        "name",
+        "address",
+        "price",
+        "pets_allowed",
+    )
+
+    list_filter = (
+
+        "price",
+        "pets_allowed",
+
+    )
+
+    search_fields = (
+        "name__startswith",
+        "address__startswith",
+        "price",
+
+    )
+
+    list_display_links = (
+        "name",
+
+    )
+
+    # 반려 동물 동반 가능 여부 수정을 리스트에서도 가능.
+    list_editable = (
+        "pets_allowed",
+    )
--- house/models.py
+++ house/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+# 모델은 어플리케이션에서 데이터의 모양을 묘사하는 것이다.
+# 모델로 데이터를 설명할 수 있어야 한다.
+# 어떤 종류의 데이터이고 , 다른 앱들과는 어떤 상호작용을 할 수 있는가.
+# PositiveIntegerField() 양수만.
+
+
+class House(models.Model):
+
+    '''   House model Definition   '''
+
+    name = models.CharField(
+        max_length=100,
+    )
+    price = models.PositiveIntegerField(
+        help_text="하루 이용 금액",
+    )
+    description = models.TextField(
+
+    )
+    address = models.CharField(
+        max_length=150,
+    )
+    pets_allowed = models.BooleanField(
+        verbose_name="반려동물 동반 가능 여부",
+        help_text="반려동물 동반 가능하면 체크하세요.",
+        default=True,
+    )
+
+    def __str__(self):
+        return self.name
