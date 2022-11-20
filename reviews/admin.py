ff50285c7bf21f67ed7a0756d0465178fac92d65
timeiscoal <timeiscoal@gmail.com>
2022-11-20 18:42:22 +0900

    refactor reviews admin filter

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,6 +3,30 @@ from .models import Review
 # Register your models here.
 
 
+# 22.11.20
+class WordFilter(admin.SimpleListFilter):
+
+    title = "Filter by words"
+
+    parameter_name = "word"
+
+    def lookups(self, request, models_admin):
+        return [
+            ("최고", "최고"),
+            ("grate", "Grate"),
+            ("wow", "WoW"),
+        ]
+
+    def queryset(self, request, reviews):
+        choice = self.value()
+        if choice == None:
+            return reviews.all()
+        elif choice == "최고":
+            return reviews.filter(rating__lt=100)
+        elif choice == "grate" or "wow":
+            return reviews.filter(payload__contains=choice)
+
+
 @admin.register(Review)
 class ReviewsAdmin(admin.ModelAdmin):
 
@@ -12,5 +36,8 @@ class ReviewsAdmin(admin.ModelAdmin):
     )
 
     list_filter = (
+        WordFilter,
         "rating",
+        "user__is_host",
+        "room__category",
     )
